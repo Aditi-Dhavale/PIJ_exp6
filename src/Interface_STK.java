d868416a36b721c0f31f8afa326879308f5d8f51
Aditi-Dhavale <aditidhavale22@gmail.com>
2024-02-16 23:14:23 +0530

    Initial commit

---
+++ src/Interface_STK.java
@@ -0,0 +1,9 @@
+//package assignment_6;
+
+public interface Interface_STK {
+    public void push(int element);
+    public int pop();
+    public void displayStack();
+    public boolean isOverflow();
+    public boolean isUnderflow();
+}
